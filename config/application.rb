8666e4b0fdc192881d7bdd7721dbe15f4345c6aa
Jimmy Florez <florezs043@gmail.com>
2024-11-05 15:14:18 -0500

    Sprockets config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require 'sprockets/railtie' #Help to load sprockets
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
